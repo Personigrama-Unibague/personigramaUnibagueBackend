b5ce287ca98d7d6092ef48764032aac4c5e388d4
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-03-08 10:50:45 -0500

    Initialization

---
+++ src/main/java/unibague/personigramaunibaguebackend/PersonigramaunibaguebackendApplication.java
@@ -0,0 +1,13 @@
+package unibague.personigramaunibaguebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonigramaunibaguebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonigramaunibaguebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unibague/personigramaunibaguebackend/PersonigramaunibaguebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unibague.personigramaunibaguebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonigramaunibaguebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
