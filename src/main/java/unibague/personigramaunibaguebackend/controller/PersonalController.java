0dfcd2970d8d71864363bf38535cbf1dd9a0d392
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-05-11 22:42:15 -0500

    Creacion metodo personasByUnidad

--- src/main/java/unibague/personigramaunibaguebackend/controller/PersonalController.java
+++ src/main/java/unibague/personigramaunibaguebackend/controller/PersonalController.java
@@ -1,10 +1,7 @@
 package unibague.personigramaunibaguebackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import unibague.personigramaunibaguebackend.model.Personal;
 import unibague.personigramaunibaguebackend.model.Unidad;
 import unibague.personigramaunibaguebackend.services.PersonalService;
@@ -23,5 +20,10 @@ public class PersonalController {
     @GetMapping("/getPersonal")
     public List<Personal> getPersonal() throws Exception{
         return personalService.getPersonas();
+    }
+
+    @GetMapping("/getPersonalByUnidad/{id}")
+    public List<Personal> getPersonal(@PathVariable String id) throws Exception{
+        return personalService.getPersonasByUnidad(id);
     }
 }
--- src/main/java/unibague/personigramaunibaguebackend/services/PersonalService.java
+++ src/main/java/unibague/personigramaunibaguebackend/services/PersonalService.java
@@ -10,6 +10,7 @@ import unibague.personigramaunibaguebackend.model.Unidad;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,6 +31,41 @@ public class PersonalService {
             e.printStackTrace();
         }
         return personas;
+    }
+
+    public List<Personal> getPersonasByUnidad(String id) {
+        List<Personal> personas = null;
+        List<Personal> personasByUnidad = new ArrayList<>();
+        Personal persona = new Personal();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            personas = objectMapper.readValue(new File("./src/main/resources/static/personal.json"), new TypeReference<List<Personal>>() {});
+
+            for(int i = 0; i < personas.size(); i++){
+                if(personas.get(i).getUnidad().equals(id)){
+                    persona.setNombre(personas.get(i).getNombre());
+                    persona.setCargo(personas.get(i).getCargo());
+                    persona.setCorreo(personas.get(i).getCorreo());
+                    persona.setTelefono(personas.get(i).getTelefono());
+                    persona.setExtension(personas.get(i).getExtension());
+                    persona.setFoto(personas.get(i).getFoto());
+                    persona.setUnidad(personas.get(i).getUnidad());
+
+                    personasByUnidad.add(persona);
+                }
+            }
+            return personasByUnidad;
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
