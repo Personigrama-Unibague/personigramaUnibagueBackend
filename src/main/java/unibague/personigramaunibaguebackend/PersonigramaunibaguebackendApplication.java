9cbacf1a50172de66e2616dcbfc533eb2342011d
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-07-31 11:11:08 -0500

    Mod

--- src/main/java/unibague/personigramaunibaguebackend/PersonigramaunibaguebackendApplication.java
+++ src/main/java/unibague/personigramaunibaguebackend/PersonigramaunibaguebackendApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,16 +12,6 @@ public class PersonigramaunibaguebackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonigramaunibaguebackendApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:9090").allowedMethods("*").allowedHeaders("*");
-            }
-        };
     }
 
     @Bean
